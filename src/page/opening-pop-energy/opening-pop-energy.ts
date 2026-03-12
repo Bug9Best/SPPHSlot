5d6ba3f60499f25e3bcfb977c7f7dc402c93ea1e
Natthaphon Ditthaviboon <bug9best@gmail.com>
2026-03-12 17:10:23 +0700

    done opening pop energy

---
+++ src/page/opening-pop-energy/opening-pop-energy.ts
@@ -0,0 +1,59 @@
+import { Component, inject, signal } from '@angular/core';
+import { Firestore, doc, updateDoc, onSnapshot, docData } from '@angular/fire/firestore';
+
+interface CeremonyState {
+  energy: number;
+  maxEnergy?: number;
+  vipScanned: boolean;
+  ceremonyStarted: boolean;
+}
+
+@Component({
+  selector: 'app-opening-pop-energy',
+  imports: [],
+  templateUrl: './opening-pop-energy.html',
+  styleUrl: './opening-pop-energy.scss',
+})
+export class OpeningPopEnergy {
+
+  private firestore: Firestore = inject(Firestore);
+  private docRef = doc(this.firestore, 'ceremony/status');
+
+  isReady = signal<boolean>(false);
+  isPressing = signal<boolean>(false);
+
+  state = signal<CeremonyState>({
+    energy: 0,
+    maxEnergy: 1000,
+    vipScanned: false,
+    ceremonyStarted: false
+  });
+
+  ngOnInit() {
+    onSnapshot(this.docRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.data() as CeremonyState;
+        this.state.set(data);
+        this.isReady.set(true);
+      } else {
+        console.error("หา Document ไม่เจอใน Firestore!");
+      }
+    });
+  }
+
+  async handleUserClick() {
+    if (!this.isReady()) return;
+
+    const currentEnergy = this.state().energy;
+    const maxEnergy = this.state().maxEnergy ?? 1000;
+    if (currentEnergy < maxEnergy) {
+      try {
+        await updateDoc(this.docRef, {
+          energy: currentEnergy + 1
+        });
+      } catch (e) {
+        console.error("Update error", e);
+      }
+    }
+  }
+}
