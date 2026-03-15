567e2890ba64fdff9f00e224f1219e191c83b66e
Natthaphon Ditthaviboon <bug9best@gmail.com>
2026-03-15 20:18:44 +0700

    update home

--- src/page/home/home.ts
+++ src/page/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-home',
   imports: [
     ButtonModule,
-    RouterLink
+    RouterLink,
+    DividerModule
 ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
