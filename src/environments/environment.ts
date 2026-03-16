0d279b7d13d4cf704390d29e10924c33f4fc04df
Natthaphon Ditthaviboon <bug9best@gmail.com>
2026-03-16 10:13:25 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,18 @@
 export const environment = {
     firebase: {
-        apiKey: "AIzaSyBZm3A9PdW1gFvMuRI7W-MY83RX8GhtjuQ",
-        authDomain: "spphslot.firebaseapp.com",
+        // apiKey: "AIzaSyBZm3A9PdW1gFvMuRI7W-MY83RX8GhtjuQ",
+        // authDomain: "spphslot.firebaseapp.com",
         databaseURL: "https://spphslot-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "spphslot",
-        storageBucket: "spphslot.firebasestorage.app",
-        messagingSenderId: "812011846983",
-        appId: "1:812011846983:web:f851d8c5a0d7a599ace062"
+        // projectId: "spphslot",
+        // storageBucket: "spphslot.firebasestorage.app",
+        // messagingSenderId: "812011846983",
+        // appId: "1:812011846983:web:f851d8c5a0d7a599ace062",
+
+        apiKey: "AIzaSyBX7F7kXpeVQ0Gel2ttiq1I3Od_0bnah4c",
+        authDomain: "spphluckydraw.firebaseapp.com",
+        projectId: "spphluckydraw",
+        storageBucket: "spphluckydraw.firebasestorage.app",
+        messagingSenderId: "665584799163",
+        appId: "1:665584799163:web:00bc451daa6e3830d3c89e"
     }
 };
